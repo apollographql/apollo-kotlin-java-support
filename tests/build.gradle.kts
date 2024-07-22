a911e8cc158e40a7e0f0d44ccc366e45ed881e73
BoD <BoD@JRAF.org>
2024-07-22 17:38:54 +0200

    Fix tests included build

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -2,7 +2,11 @@ buildscript {
   repositories {
     mavenCentral()
   }
-  dependencies {
-    classpath("build-logic:build-logic")
-  }
+}
+
+plugins {
+  alias(libs.plugins.kotlin).apply(false)
+  alias(libs.plugins.apollo).apply(false)
+  alias(libs.plugins.ksp).apply(false)
+  alias(libs.plugins.apollo.execution).apply(false)
 }
